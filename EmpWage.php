d3d77b0c992e1ca4713df9a9740e228c9d7d1cef
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-12-22 22:48:27 +0530

    UC5 added

--- EmpWage.php
+++ EmpWage.php
@@ -1,25 +1,29 @@
-<?php
-echo "Welcome to Employee Wage Problem" . "\n";
+<?php 
+echo "Welcome to Employee Wage Problem"."\n";
 
-//variables
 $WAGE_PER_HR = 20;
+$MAX_WORKING_DAYS = 20;
+$TOTAL_WORKING_DAYS = 0;
+$TOTAL_EMP_HRS = 0;
 $EMP_HRS;
-//using rand() function which will print 0,1,2
-$num = rand(0,2);
 
-//using switch cases to print values
-switch($num){
-    case 1 :
-        $EMP_HRS = 8;
-        break;
-    case 2 :
-        $EMP_HRS = 4;
-        break;
-    default :
-        $EMP_HRS = 0;         
+while ($TOTAL_WORKING_DAYS < $MAX_WORKING_DAYS) {
+    $num = rand(0,2);
+    switch($num){
+        case 1 :
+            $EMP_HRS = 8;
+            break;
+        case 2 :
+            $EMP_HRS = 4;
+            break;
+        default :
+            $EMP_HRS = 0;
+    }
+
+    $TOTAL_EMP_HRS = $TOTAL_EMP_HRS + $EMP_HRS;
+    $TOTAL_WORKING_DAYS++;
 }
 
-
-$TotalWageInDay = $WAGE_PER_HR * $EMP_HRS;
-echo "Total Wage in Day : " .$TotalWageInDay;
+$TOTAL_SALARY = $WAGE_PER_HR * $TOTAL_EMP_HRS;
+echo "Total Salary : " . $TOTAL_SALARY;
 ?>
