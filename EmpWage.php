d7ef1452764ff6ed9e0120497c126bae08d7e666
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2022-01-05 01:31:25 +0530

    Added

--- EmpWage.php
+++ EmpWage.php
@@ -1,30 +1,42 @@
 <?php
 echo "Welcome to Employee Wage Problem <br>";
 
-$WAGE_PER_HR = 20;
-$MAX_WORKING_DAYS = 22;
-$MAX_HRS_IN_MONTH = 100;
-$TOTAL_WORKING_DAYS = 0;
-$TOTAL_EMP_HRS = 0;
-$EMP_HRS;
+class EmpWage {
 
-while ($TOTAL_WORKING_DAYS < $MAX_WORKING_DAYS && $TOTAL_EMP_HRS < $MAX_HRS_IN_MONTH) {
-    $num = rand(0,2);
-    switch($num){
-        case 1 :
-            $EMP_HRS = 8;
-            break;
-        case 2 :
-            $EMP_HRS = 4;
-            break;
-        default :
-            $EMP_HRS = 0;
+    public $IS_FULL_TIME = 1;
+    public $IS_PART_TIME = 2;
+    public $IS_ABSENT = 0;
+    public $WAGE_PER_HR = 20;
+    public $MAX_WORKING_DAYS = 22;
+    public $MAX_HRS_IN_MONTH = 100;
+    public $EMP_HRS;
+
+    function calculateEmployeeWage() {
+        
+        $TOTAL_WORKING_DAYS = 0;
+        $TOTAL_EMP_HRS = 0;
+        while ($TOTAL_WORKING_DAYS < $this->MAX_WORKING_DAYS && $TOTAL_EMP_HRS < $this->MAX_HRS_IN_MONTH) {
+            $num = rand(0, 2);
+            switch ($num) {
+                case 1:
+                    $EMP_HRS = 8;
+                    break;
+                case 2:
+                    $EMP_HRS = 4;
+                    break;
+                default:
+                    $EMP_HRS = 0;
+            }
+
+            $TOTAL_EMP_HRS = $TOTAL_EMP_HRS + $EMP_HRS;
+            $TOTAL_WORKING_DAYS++;
+        }
+
+        $TOTAL_SALARY = $this->WAGE_PER_HR * $TOTAL_EMP_HRS;
+        echo "Total Salary :" .$TOTAL_SALARY;
     }
-
-    $TOTAL_EMP_HRS = $TOTAL_EMP_HRS + $EMP_HRS;
-    $TOTAL_WORKING_DAYS++;
 }
 
-$TOTAL_SALARY = $WAGE_PER_HR * $TOTAL_EMP_HRS;
-echo "Total Salary : " . $TOTAL_SALARY;
+$wage = new EmpWage();
+$wage->calculateEmployeeWage();
 ?>
